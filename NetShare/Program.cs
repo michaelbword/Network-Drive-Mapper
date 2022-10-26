3f534cbdfe9be2757cf918ae786f0aface8d176a
Blake Word <michaelbword@gmail.com>
2022-10-26 15:11:51 -0500

    Update Program.cs

--- NetShare/Program.cs
+++ NetShare/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System.Numerics;
 
 Console.WriteLine("Mapping the H drive... Please wait");
-Utility.NetworkDrive.MapNetworkDrive("H", @"\\fileserver\public");
+Utility.NetworkDrive.MapNetworkDrive("H", @"\\REDACTED");
 Thread.Sleep(2000);
 var dirs1 = Directory.GetDirectories("H:");
 Console.WriteLine("H drive mapped!");
 Thread.Sleep(2000);
 Console.WriteLine("Mapping the J drive... Please wait");
-Utility.NetworkDrive.MapNetworkDrive("J", @"\\fileserver\department folders");
+Utility.NetworkDrive.MapNetworkDrive("J", @"\\REDACTED");
 Thread.Sleep(2000);
 var dirs2 = Directory.GetDirectories("J:");
-Console.WriteLine("Drive mapping complete, if you encounter an issue please email Officesupport@mapcoexpress.com");
+Console.WriteLine("Drive mapping complete, if you encounter an issue please email REDACTED");
 Thread.Sleep(5000);
